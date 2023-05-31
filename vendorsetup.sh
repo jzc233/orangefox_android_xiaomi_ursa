6867000711dd7cc0af2b48859d421819d3944bea
Darth9 <reyprimerjefe@gmail.com>
2023-05-31 18:03:49 +0100

    This script requires bash

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2018-2022 The OrangeFox Recovery Project
+# 	Copyright (C) 2018-2023 The OrangeFox Recovery Project
 #	
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -39,5 +39,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 	# if we are not using a stock kernel, disable LZMA compression   
 	[ "$FOX_USE_STOCK_KERNEL" != "1" ] && unset FOX_USE_LZMA_COMPRESSION 
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
 fi
 #
