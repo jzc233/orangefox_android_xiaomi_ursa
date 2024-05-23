7b70f5bd657fc49e80ab5426c9a81ba6197215aa
Darth9 <reyprimerjefe@gmail.com>
2024-05-23 14:23:48 +0100

    Clone the common repo if necessary (replace twrp.dependencies)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2018-2023 The OrangeFox Recovery Project
+# 	Copyright (C) 2018-2024 The OrangeFox Recovery Project
 #	
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -19,6 +19,19 @@
 #
 FDEVICE="dipper"
 
+fetch_sdm845_common_repo() {
+	local URL=https://gitlab.com/OrangeFox/device/sdm845-common.git;
+	local common=device/xiaomi/sdm845-common;
+	local branch=fox_12.1;
+
+	if [ ! -d $common ]; then
+		echo "Cloning $URL ... to $common";
+		git clone $URL -b $branch $common;
+	else
+		echo "Device common repository: \"$common\" found ...";
+	fi
+}
+
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
    if [ -n "$chkdev" ]; then
@@ -34,10 +47,13 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	# pull in the common vendorsetup.sh
-   	source device/xiaomi/sdm845-common/vendorsetupcommon.sh
+	# clone the common repo if necessary
+	fetch_sdm845_common_repo;
 
-	# if we are not using a stock kernel, disable LZMA compression   
+	# pull in the common vendorsetup.sh
+	source device/xiaomi/sdm845-common/vendorsetupcommon.sh
+
+	# if we are not using a stock kernel, disable LZMA compression
 	[ "$FOX_USE_STOCK_KERNEL" != "1" ] && unset OF_USE_LZMA_COMPRESSION
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
