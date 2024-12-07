83c9e3dd081fc81be0c9bf4ca38937bb68719fdb
jzc233 <63482703+jzc233@users.noreply.github.com>
2024-12-08 01:43:46 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -27,6 +27,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter dipper,$(TARGET_DEVICE)),)
+ifneq ($(filter equuleus,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
